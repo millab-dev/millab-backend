8e7871ee8aa78fe611bfee5d0f9ee7ce02c07d9d
isaui <isadestroyed17@gmail.com>
2025-06-05 23:04:19 +0700

    y

--- src/index.ts
+++ src/index.ts
@@ -23,6 +23,7 @@ const app = new Elysia()
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     credentials: true // Allow credentials (cookies)
   }))
+  .use(authMiddleware)
   // Mount all API routes
   .use(apiRoutes)
   
